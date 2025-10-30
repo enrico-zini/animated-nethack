e24e40d3bbb09e9fe0e41442ced5d6c49c2e5555
Enrico Zini <enricoassmannzini@gmail.com>
2025-10-30 19:05:25 -0300

    refactor: player structure

--- main.c
+++ main.c
@@ -14,18 +14,35 @@
 int windowWidth = GRID_WIDTH;
 int windowHeight = GRID_HEIGHT;
 
-typedef struct Coord2D {
+typedef struct {
 	float x;
 	float y;
-} Coord2D;
+} Vector2f;
 
-typedef struct Player {
-	Coord2D pos;
-	Coord2D start;
-	Coord2D dir;
-} Player;
+typedef struct {
+	int x;
+	int y;
+} Vector2i;
 
-Player player = {{0, 0}, {0, 0}, {0, 0}};
+typedef struct {
+	Vector2f global_pos;
+	Vector2i tile_pos;
+	Vector2i dir;
+} Movable;
+
+typedef struct {
+	Movable mov;
+	Vector2i path[9];
+} Npc;
+
+Npc npc = {
+    .mov = {{0.0f, 0.0f}, {0.0f, 0.0f}, {0.0f, 0.0f}},
+    .path = {
+    	{10, 10}, {11, 10}, {12, 10}, {12, 9}, {12, 8}, {11, 8}, {10, 8}, {10, 9}, {10, 10} 
+    }
+};
+
+Movable player = {{0.0f, 0.0f}, {0, 0}, {0, 0}};
 
 // ANIMATION
 int animation_counter = ANIMATION_STEPS;
@@ -48,22 +65,22 @@ void drawGrid() {
 }
 
 void animatePlayer() {
-	player.pos.x = player.start.x + (player.dir.x * animation_counter/ANIMATION_STEPS);
-	player.pos.y = player.start.y + (player.dir.y * animation_counter/ANIMATION_STEPS);
+	player.global_pos.x = (player.tile_pos.x + (float)player.dir.x * animation_counter/ANIMATION_STEPS) * TILE_SIZE;
+	player.global_pos.y = (player.tile_pos.y + (float)player.dir.y * animation_counter/ANIMATION_STEPS) * TILE_SIZE;
 	animation_counter++;
 	if (animation_counter == ANIMATION_STEPS) { // ANIMATION END
-		player.start.x += player.dir.x;
-		player.start.y += player.dir.y;
+		player.tile_pos.x += player.dir.x;
+		player.tile_pos.y += player.dir.y;
 
-		player.pos.x = player.start.x;
-		player.pos.y = player.start.y;
+		player.global_pos.x = player.tile_pos.x * TILE_SIZE;
+		player.global_pos.y = player.tile_pos.y * TILE_SIZE;
 	}
 }
 
 void drawPlayer() {
     glColor3f(WHITE);
-    float x = player.pos.x * TILE_SIZE;
-    float y = player.pos.y * TILE_SIZE;
+    float x = player.global_pos.x;
+    float y = player.global_pos.y;
 	glBegin(GL_QUADS);
 		glVertex2i(x, y);
 		glVertex2i(x + TILE_SIZE, y);
@@ -122,20 +139,20 @@ void take_input(int key, int x, int y) {
 	
     switch (key) {
         case GLUT_KEY_UP:
-            player.dir.x = 0.0f;
-            player.dir.y = 1.0f;
+            player.dir.x = 0;
+            player.dir.y = 1;
             break;
         case GLUT_KEY_DOWN:
-            player.dir.x = 0.0f;
-            player.dir.y = -1.0f;
+            player.dir.x = 0;
+            player.dir.y = -1;
             break;
         case GLUT_KEY_LEFT:
-            player.dir.x = -1.0f;
-            player.dir.y = 0.0f;
+            player.dir.x = -1;
+            player.dir.y = 0;
             break;
         case GLUT_KEY_RIGHT:
-            player.dir.x = 1.0f;
-            player.dir.y = 0.0f;
+            player.dir.x = 1;
+            player.dir.y = 0;
             break;
         default:
             return;
